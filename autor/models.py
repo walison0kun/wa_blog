c52610dffd4c0ee477803b86ca5ec0b27b475ffe
walison0kun <francisco.w.a.lopes@gmail.com>
2019-12-04 19:40:31 -0300

    Migrando o modelo Autor

--- autor/models.py
+++ autor/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class author(models.Model):
     nome = models.CharField(max_length=255, null=False, blank=False)
-    idade = models.IntegerField(max_length=2)
+    idade = models.IntegerField()
     sexo = models.TextField(max_length=10, null=False)
     e_mail = models.EmailField(max_length=254, null=False)
     resu_perfil = models.CharField(max_length=255)
