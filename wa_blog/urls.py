6c6c69de134da6d79947ce85b90869db471cfcf6
walison0kun <francisco.w.a.lopes@gmail.com>
2019-12-03 22:20:26 -0300

    adicionando na HomePage

--- wa_blog/urls.py
+++ wa_blog/urls.py
@@ -6,11 +6,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 from.import views
+from blog import views as blog_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('autor/', include('autor.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', views.home),
+    path('', blog_views.post_list, name="home"),
     path('about/', views.about),
     path('blog/', include('blog.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
