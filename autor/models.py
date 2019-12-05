be5d8d20b9e56320a6cabeb0a6f1b42731c6eb27
walison0kun <francisco.w.a.lopes@gmail.com>
2019-12-05 00:12:07 -0300

    alterando a camada models de autor

--- autor/models.py
+++ autor/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class author(models.Model):
     nome = models.CharField(max_length=255, null=False, blank=False)
-    idade = models.IntegerField()
+    idade = models.CharField(max_length=2)
     sexo = models.TextField(max_length=10, null=False)
     e_mail = models.EmailField(max_length=254, null=False)
     resu_perfil = models.CharField(max_length=255)
