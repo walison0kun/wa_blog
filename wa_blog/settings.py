d5c4954dc4184105d3ca688de0a030524e6e0a39
walison0kun <francisco.w.a.lopes@gmail.com>
2019-12-05 00:09:20 -0300

    finalizando o layout com css

--- wa_blog/settings.py
+++ wa_blog/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'wa_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
